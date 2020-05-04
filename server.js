10a422a0a8857f8ecb7ad1b3b5be2d823fb332aa
hanaakhoj <you@example.com>
2020-05-04 21:42:36 +0000

    Database connection

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// databse
+const db = require('./config/database');
+
+// db connection
+db.authenticate()
+    .then(() => console.log('Database connected successfully.'))
+    .catch(err => console.error('Error: ', err));
+
 app.get('/', (req, res) => res.send('TODOLIST'));
 
 app.listen(port, () => {
